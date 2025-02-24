3611d232aa5ecd5d38a5a99ed596a944b6cc81e0
706ash <carameloco7@gmail.com>
2025-02-24 18:42:34 +0530

    ..

--- student_attendence_backend/accounts/admin.py
+++ student_attendence_backend/accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import CustomUser  # Import your custom user model
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    # Define the fields to be displayed in the Django admin panel
+    list_display = ('email', 'full_name', 'profession', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'profession')
+    search_fields = ('email', 'full_name')
+    ordering = ('email',)
+
+    fieldsets = (
+        (_("Personal Info"), {"fields": ("email", "full_name", "profession", "password")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Important Dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (_("Create User"), {
+            "classes": ("wide",),
+            "fields": ("email", "full_name", "profession", "password1", "password2"),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
