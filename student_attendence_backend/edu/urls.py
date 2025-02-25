6c646740019a6c7f18a1482f99f3b7461e0b5b2d
706ash <carameloco7@gmail.com>
2025-02-25 07:47:36 +0530

    ..

--- student_attendence_backend/edu/urls.py
+++ student_attendence_backend/edu/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('student/', views.student_dashboard, name = "student_dashboard"),
     path('teacher/', views.teacher_dashboard, name = "teacher_dashboard"),
     path('stu_add/', views.stu_add, name = "stu_add"),
-    # path('logout/', views.logout_view, name = "logout"),
+    
 ]
