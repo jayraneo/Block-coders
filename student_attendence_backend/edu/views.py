c6a378ef3d7dd65608d01a3145bbc10ad9773c83
706ash <carameloco7@gmail.com>
2025-02-25 07:04:15 +0530

    Update views.py

--- student_attendence_backend/edu/views.py
+++ student_attendence_backend/edu/views.py
@@ -2,8 +2,9 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
 from django.utils.timezone import now
-from django.shortcuts import render
-from .models import Attendance, Subject, StudentProfile
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Attendance, Subject, StudentProfile, User
 
 @login_required
 def student_dashboard(request):
